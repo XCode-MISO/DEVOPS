25354afe402ac539b63cab15a75b341de46f9da8
Andres Felipe Losada <nedrocoli@gmail.com>
2024-10-12 21:49:36 -0500

    finishing get endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request
 
-from src.views.views  import post_add_email_to_blacklist
+from src.views.views  import post_add_email_to_blacklist, get_blacklisted_entries
 from src.config.app_config import config_app, init_db, database, APP_DEBUG, APP_PORT
 
 # Load environment variables
@@ -22,6 +22,10 @@ def check_service():
 @application.route("/blacklists", methods=["POST"])
 def add_email_to_blacklist():
     return post_add_email_to_blacklist(database, request)
+
+@application.route("/blacklists/<email>", methods=["GET"])
+def get_blacklisted_entries_route(email):
+    return get_blacklisted_entries(database, request, email)
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", debug=APP_DEBUG, port=APP_PORT)
